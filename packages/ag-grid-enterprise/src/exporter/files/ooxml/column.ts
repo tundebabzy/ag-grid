fe5234ac6fb7c5aa44e6d30f95c9303f6c207d4b
Guilherme Lopes <guilherme@gwm-solutions.com>
2018-09-22 00:12:48 +0100

    added code to merge cells from mergeAcross

--- packages/ag-grid-enterprise/src/exporter/files/ooxml/column.ts
+++ packages/ag-grid-enterprise/src/exporter/files/ooxml/column.ts
@@ -1,8 +1,10 @@
 import {ExcelOOXMLTemplate, ExcelColumn} from 'ag-grid-community';
 
+const getExcelCellWidth = (width: number): number => Math.max(Math.ceil((width - 12) / 7 + 1), 10);
+
 const column: ExcelOOXMLTemplate = {
     getTemplate(config: ExcelColumn) {
-        const {min, max, width = 10, s, hidden, bestFit} = config;
+        const {min, max, s, width, hidden, bestFit} = config;
 
         return {
             name: 'col',
@@ -10,7 +12,7 @@ const column: ExcelOOXMLTemplate = {
                 rawMap: {
                     min,
                     max,
-                    width,
+                    width: !width ? 10 : getExcelCellWidth(width),
                     style: s,
                     hidden: hidden ? '1' : '0',
                     bestFit: bestFit ? '1' : '0',
--- packages/ag-grid-enterprise/src/exporter/files/ooxml/worksheet.ts
+++ packages/ag-grid-enterprise/src/exporter/files/ooxml/worksheet.ts
@@ -1,11 +1,53 @@
-import {ExcelOOXMLTemplate, ExcelWorksheet, _} from 'ag-grid-community';
+import {ExcelOOXMLTemplate, ExcelWorksheet, ExcelRow, ExcelCell, _} from 'ag-grid-community';
 import column from './column';
 import row from './row';
+import mergeCell from './mergeCell';
+
+const getMergedCells = (rows: ExcelRow[]): string[] => {
+    const mergedCells: string[] = [];
+
+    rows.forEach((currentRow, rowIdx) => {
+        const cells = currentRow.cells;
+        let merges = 0;
+        cells.forEach((currentCell, cellIdx) => {
+            const start = getExcelColumnName(cellIdx + merges + 1);
+            const outputRow = rowIdx + 1;
+
+            if (currentCell.mergeAcross) {
+                merges += currentCell.mergeAcross;
+                const end = getExcelColumnName(cellIdx + merges + 1);
+
+                mergedCells.push(`${start}${outputRow}:${end}${outputRow}`);
+            }
+
+            currentCell.ref = `${start}${outputRow}`;
+        });
+    });
+
+    return mergedCells;
+};
+
+const getExcelColumnName = (colIdx: number): string => {
+    const startCode = 65;
+    const tableWidth = 26;
+    const fromCharCode = String.fromCharCode;
+
+    const pos = Math.floor(colIdx / tableWidth);
+    const tableIdx = colIdx % tableWidth;
+
+    if (!pos || colIdx === tableWidth) return fromCharCode(startCode + colIdx - 1);
+    if (!tableIdx) return getExcelColumnName(pos - 1) + 'Z';
+    if (pos < tableWidth) return fromCharCode(startCode + pos - 1) + fromCharCode(startCode + tableIdx - 1);
+
+    return getExcelColumnName(pos) + fromCharCode(startCode + tableIdx - 1);
+};
 
 const worksheet: ExcelOOXMLTemplate = {
     getTemplate(config: ExcelWorksheet) {
         const {table} = config;
         const {rows, columns} = table;
+
+        const mergedCells = getMergedCells(rows);
 
         const children = [].concat(
             columns.length ? {
@@ -16,6 +58,16 @@ const worksheet: ExcelOOXMLTemplate = {
             rows.length ? {
                 name: 'sheetData',
                 children: _.map(rows, row.getTemplate)
+            } : []
+        ).concat(
+            mergedCells.length ? {
+                name: 'mergeCells',
+                properties: {
+                    rawMap: {
+                        count: mergedCells.length
+                    }
+                },
+                children: _.map(mergedCells, mergeCell.getTemplate)
             } : []
         );
 
